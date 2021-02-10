09cfcd54d1fe472305f014c4659e988c98c3c790
Stelian Grosu <stelian.email@yahoo.com>
2021-02-10 12:25:14 -0500

    update2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App" >
-      </div>
-    );
+
+
+console.log(contacts)
+
+function App() {
+  const [stateCelbs, setStateCelbs] = useState(contacts.splice(0, 5))
+  const [otherCelbs, setOtherCelbs] = useState(contacts)
+  const showFive = () => {
+    return stateCelbs.map(eachContact => {
+      return <li>{eachContact.name} <img src={eachContact.pictureUrl} /></li>
+    })
   }
+
+  const addRandom = () => {
+    console.log('hello')
+    let randomN = Math.floor(Math.random() * otherCelbs.length)
+    let randomCeleb = otherCelbs.splice(randomN, 1)[0]
+    let newCelebs = [...stateCelbs]
+
+    newCelebs.push({ ...randomCeleb })
+
+    console.log(randomCeleb, newCelebs)
+    setStateCelbs(newCelebs)
+  }
+
+
+  return (
+    <div>Hello
+      {showFive()}
+      <button onClick={addRandom}>Add Rendom Actor</button>
+    </div>
+  )
 }
 
 export default App;
